2c609bd9d8c7a7ddb27e7a83655e0ab3b57a021b
Matt Empson <empson.matthew@gmail.com>
2020-02-04 13:15:31 +1100

    Added additional resources and tasks

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Python script that uses the Troposphere library to generate a CloudFormation template that will deploy a broken environment.
 
-Deploy the CloudFormation template and then fix the problems, so you can SSH into the EC2 instance and install the latest OS updates.
+Deploy the CloudFormation template and then fix any problems, so you can:
+* SSH into the EC2 JumpBox instance and install the latest OS updates.
+* From the JumpBox, SSH into the S3 instance, create a text file with your name and upload it to "namebucket" via the CLI
+* Upload the index.html file to the "cloudfrontbucket" and have is accessible via the cloudfront url e.g http://d3outmmle9mf6g.cloudfront.net/.....
--- generate_template.py
+++ generate_template.py
@@ -1,13 +1,24 @@
 #!/bin/env python
 
 from troposphere import GetAZs, Join, Parameter, Ref, Select, Tags, Template
+from awacs.aws import Allow, Condition, Policy, Principal, Statement, Action
+from awacs.aws import IpAddress
+from awacs.sts import AssumeRole
+import random
+import troposphere.cloudfront as cloudfront
 import troposphere.ec2 as ec2
+import troposphere.iam as iam
+import troposphere.s3 as s3
 
 if __name__ == "__main__":
 
     t = Template()
 
     ami = "ami-0b8b10b5bf11f3a22"
+
+    buckets = ["nameBucket", "cloudfrontBucket"]
+
+    natAmi = "ami-0f113fc1f0244a121"
 
     name = t.add_parameter(
         Parameter(
@@ -42,9 +53,195 @@ if __name__ == "__main__":
         ec2.InternetGateway("IGW", Tags=Tags(Name=Join("", [Ref(name), "-IGW"])))
     )
 
+    # Jump box security group
+    securityGroup = t.add_resource(
+        ec2.SecurityGroup(
+            "SecurityGroup",
+            GroupDescription="Jump-Box-SG",
+            GroupName="jumpbox",
+            VpcId=Ref(vpc),
+            Tags=Tags(Name=Join("", [Ref(name), "-jump-box"])),
+        )
+    )
+
+    securityGroupINSSH = t.add_resource(
+        ec2.SecurityGroupIngress(
+            "SGSSHAllow",
+            Description=Join("", [Ref(name), "s source IP"]),
+            CidrIp=Join("", [Ref(sshAddress), "/32"]),
+            FromPort=22,
+            ToPort=22,
+            IpProtocol="6",
+            GroupId=Ref(securityGroup),
+        )
+    )
+
+    securityGroupOUTICMP = t.add_resource(
+        ec2.SecurityGroupEgress(
+            "SGICMPAllow",
+            CidrIp="0.0.0.0/0",
+            FromPort=-1,
+            ToPort=-1,
+            IpProtocol="1",
+            GroupId=Ref(securityGroup),
+        )
+    )
+
+    # NAT instance security group
+    natSecurityGroup = t.add_resource(
+        ec2.SecurityGroup(
+            "NatSecurityGroup",
+            GroupDescription="NAT-SG",
+            GroupName="nat",
+            VpcId=Ref(vpc),
+            Tags=Tags(Name=Join("", [Ref(name), "-nat-instance"])),
+        )
+    )
+
+    natSecurityGroupIN = t.add_resource(
+        ec2.SecurityGroupIngress(
+            "NATAllowIN",
+            Description="VPC CIDR",
+            CidrIp="10.0.0.0/16",
+            FromPort=-1,
+            ToPort=-1,
+            IpProtocol="-1",
+            GroupId=Ref(natSecurityGroup),
+        )
+    )
+
+    natSecurityGroupOUT = t.add_resource(
+        ec2.SecurityGroupEgress(
+            "NATAllowOUT",
+            CidrIp="0.0.0.0/0",
+            FromPort=-1,
+            ToPort=-1,
+            IpProtocol="-1",
+            GroupId=Ref(natSecurityGroup),
+        )
+    )
+
+    # S3 instance security group
+    S3SecurityGroup = t.add_resource(
+        ec2.SecurityGroup(
+            "S3SecurityGroup",
+            GroupDescription="S3-SG",
+            GroupName="S3",
+            VpcId=Ref(vpc),
+            Tags=Tags(Name=Join("", [Ref(name), "-S3-instance"])),
+        )
+    )
+
+    S3SecurityGroupIN = t.add_resource(
+        ec2.SecurityGroupIngress(
+            "S3NATAllowIN",
+            Description="VPC CIDR",
+            CidrIp="10.0.0.0/16",
+            FromPort=-1,
+            ToPort=-1,
+            IpProtocol="-1",
+            GroupId=Ref(S3SecurityGroup),
+        )
+    )
+
+    S3SecurityGroupOUT = t.add_resource(
+        ec2.SecurityGroupEgress(
+            "S3AllowOUT",
+            CidrIp="0.0.0.0/0",
+            FromPort=-1,
+            ToPort=-1,
+            IpProtocol="-1",
+            GroupId=Ref(S3SecurityGroup),
+        )
+    )
+
+    # Jump Box
+    instance = t.add_resource(
+        ec2.Instance(
+            "JumpBox",
+            ImageId=ami,
+            InstanceType="t2.micro",
+            KeyName=Ref(sshKey),
+            SecurityGroupIds=[Ref(securityGroup)],
+            SubnetId=Ref("Subnet1"),
+            Tags=Tags(Name=Join("", [Ref(name), "-jump-box"])),
+        )
+    )
+
+    # NAT Instance
+    natInstance = t.add_resource(
+        ec2.Instance(
+            "NatInstance",
+            ImageId=natAmi,
+            InstanceType="t2.micro",
+            KeyName=Ref(sshKey),
+            SecurityGroupIds=[Ref(natSecurityGroup)],
+            SubnetId=Ref("Subnet1"),
+            Tags=Tags(Name=Join("", [Ref(name), "-nat"])),
+        )
+    )
+
+    # IAM Policy and Role for S3 instance
+    S3Policy = iam.Policy(
+        PolicyName="S3Policy",
+        PolicyDocument=Policy(
+            Statement=[
+                Statement(
+                    Sid="S3Policy",
+                    Effect=Allow,
+                    Action=[
+                        Action("s3", "DeleteObject"),
+                        Action("s3", "GetObject"),
+                        Action("s3", "PutObject"),
+                    ],
+                    Resource=["arn:aws:s3:::namebucket-*/*"],
+                    Condition=Condition([IpAddress("aws:SourceIp", "10.0.0.0/16")]),
+                )
+            ]
+        ),
+    )
+
+    S3Role = t.add_resource(
+        iam.Role(
+            "S3Role",
+            RoleName="S3Role",
+            AssumeRolePolicyDocument=Policy(
+                Statement=[
+                    Statement(
+                        Effect=Allow,
+                        Action=[AssumeRole],
+                        Principal=Principal("Service", ["ec2.amazonaws.com"]),
+                    )
+                ]
+            ),
+            Policies=[S3Policy],
+        )
+    )
+
+    S3InstanceProfile = t.add_resource(
+        iam.InstanceProfile("InstanceProfile", Roles=[Ref(S3Role)])
+    )
+
+    # S3 Instance
+    S3Instance = t.add_resource(
+        ec2.Instance(
+            "S3Instance",
+            IamInstanceProfile=Ref(S3InstanceProfile),
+            ImageId=ami,
+            InstanceType="t2.micro",
+            KeyName=Ref(sshKey),
+            SecurityGroupIds=[Ref(S3SecurityGroup)],
+            SubnetId=Ref("Subnet4"),
+            Tags=Tags(Name=Join("", [Ref(name), "-S3-instance"])),
+        )
+    )
+
+    # Public NACL
     nacl = t.add_resource(
         ec2.NetworkAcl(
-            "NACL", VpcId=Ref(vpc), Tags=Tags(Name=Join("", [Ref(name), "-NACL"]))
+            "NACL",
+            VpcId=Ref(vpc),
+            Tags=Tags(Name=Join("", [Ref(name), "-NACL-PUBLIC"])),
         )
     )
 
@@ -83,6 +280,39 @@ if __name__ == "__main__":
         )
     )
 
+    # Private NACL
+    privateNacl = t.add_resource(
+        ec2.NetworkAcl(
+            "PrivateNACL",
+            VpcId=Ref(vpc),
+            Tags=Tags(Name=Join("", [Ref(name), "-NACL-PRIVATE"])),
+        )
+    )
+
+    priavteNaclIN = t.add_resource(
+        ec2.NetworkAclEntry(
+            "PrivateInboundAllow",
+            CidrBlock="0.0.0.0/0",
+            NetworkAclId=Ref(privateNacl),
+            Protocol=-1,
+            RuleAction="allow",
+            RuleNumber=10,
+        )
+    )
+
+    privateNaclOut = t.add_resource(
+        ec2.NetworkAclEntry(
+            "PrivateOutBoundAllow",
+            CidrBlock="0.0.0.0/0",
+            Egress=True,
+            NetworkAclId=Ref(privateNacl),
+            Protocol=-1,
+            RuleAction="allow",
+            RuleNumber=10,
+        )
+    )
+
+    # Public route table
     routeTable = t.add_resource(
         ec2.RouteTable(
             "PublicRouteTable",
@@ -91,6 +321,25 @@ if __name__ == "__main__":
         )
     )
 
+    # Private route table
+    privateRouteTable = t.add_resource(
+        ec2.RouteTable(
+            "PrivateRouteTable",
+            VpcId=Ref(vpc),
+            Tags=Tags(Name=Join("", [Ref(name), "-PRIVATE"])),
+        )
+    )
+
+    privateDefaultRoute = t.add_resource(
+        ec2.Route(
+            "PrivateSubnetRoute",
+            DestinationCidrBlock="0.0.0.0/0",
+            InstanceId=Ref(natInstance),
+            RouteTableId=Ref(privateRouteTable),
+        )
+    )
+
+    # Public subnets
     for i in range(0, 3):
         subnet = t.add_resource(
             ec2.Subnet(
@@ -99,7 +348,7 @@ if __name__ == "__main__":
                 CidrBlock=f"10.0.{i}.0/24",
                 MapPublicIpOnLaunch=True,
                 VpcId=Ref(vpc),
-                Tags=Tags(Name=f"10.0.{i}.0/24"),
+                Tags=Tags(Name=f"10.0.{i}.0/24-PUBLIC"),
             )
         )
 
@@ -115,48 +364,75 @@ if __name__ == "__main__":
             )
         )
 
-    securityGroup = t.add_resource(
-        ec2.SecurityGroup(
-            "SecurityGroup",
-            GroupDescription=f"Jump-Box-SG",
-            GroupName="jumpbox",
-            VpcId=Ref(vpc),
-            Tags=Tags(Name=Join("", [Ref(name), "-jump-box"])),
+    # Private subnets
+    for i in range(4, 7):
+        privateSubnet = t.add_resource(
+            ec2.Subnet(
+                f"Subnet{i}",
+                AvailabilityZone=Select(i - 4, GetAZs(Ref("AWS::Region"))),
+                CidrBlock=f"10.0.{i}.0/24",
+                MapPublicIpOnLaunch=False,
+                VpcId=Ref(vpc),
+                Tags=Tags(Name=f"10.0.{i}.0/24-PRIVATE"),
+            )
         )
-    )
 
-    securityGroupINSSH = t.add_resource(
-        ec2.SecurityGroupIngress(
-            "SGSSHAllow",
-            Description=Join("", [Ref(name), "s source IP"]),
-            CidrIp=Join("", [Ref(sshAddress), "/32"]),
-            FromPort=22,
-            ToPort=22,
-            IpProtocol="6",
-            GroupId=Ref(securityGroup),
+        privateSubnetRouteTable = t.add_resource(
+            ec2.SubnetRouteTableAssociation(
+                f"Subnet{i}RT",
+                RouteTableId=Ref(privateRouteTable),
+                SubnetId=Ref(privateSubnet),
+            )
         )
-    )
 
-    securityGroupOUTICMP = t.add_resource(
-        ec2.SecurityGroupEgress(
-            "SGICMPAllow",
-            CidrIp="0.0.0.0/0",
-            FromPort=-1,
-            ToPort=-1,
-            IpProtocol="1",
-            GroupId=Ref(securityGroup),
+        privateSubnetNACL = t.add_resource(
+            ec2.SubnetNetworkAclAssociation(
+                f"Subnet{i}NACL",
+                NetworkAclId=Ref(privateNacl),
+                SubnetId=Ref(privateSubnet),
+            )
         )
-    )
 
-    instance = t.add_resource(
-        ec2.Instance(
-            "JumpBox",
-            ImageId=ami,
-            InstanceType="t2.micro",
-            KeyName=Ref(sshKey),
-            SecurityGroupIds=[Ref(securityGroup)],
-            SubnetId=Ref("Subnet1"),
-            Tags=Tags(Name=Join("", [Ref(name), "-jump-box"])),
+    for bucket in buckets:
+        randomPrefix = random.randrange(10 ** 15)
+
+        s3bucket = t.add_resource(
+            s3.Bucket(
+                bucket,
+                BucketName=f"{bucket.lower()}-{randomPrefix}",
+                AccessControl="Private",
+                PublicAccessBlockConfiguration=s3.PublicAccessBlockConfiguration(
+                    BlockPublicAcls=True,
+                    BlockPublicPolicy=True,
+                    IgnorePublicAcls=True,
+                    RestrictPublicBuckets=True,
+                ),
+                Tags=Tags(Name=f"{bucket}-{randomPrefix}"),
+            )
+        )
+
+        if "cloudfront" in bucket:
+            cloudfrontBucket = f"{bucket.lower()}-{randomPrefix}"
+
+    cloudfront = t.add_resource(
+        cloudfront.Distribution(
+            "Cloudfront",
+            DistributionConfig=cloudfront.DistributionConfig(
+                Origins=[
+                    cloudfront.Origin(
+                        Id="1",
+                        DomainName=f"{cloudfrontBucket}.s3-ap-southeast-2.amazonaws.com",
+                        S3OriginConfig=cloudfront.S3OriginConfig(),
+                    )
+                ],
+                DefaultCacheBehavior=cloudfront.DefaultCacheBehavior(
+                    TargetOriginId="1",
+                    ForwardedValues=cloudfront.ForwardedValues(QueryString=False),
+                    ViewerProtocolPolicy="allow-all",
+                ),
+                Enabled=True,
+                HttpVersion="http2",
+            ),
         )
     )
 
